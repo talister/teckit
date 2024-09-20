e657cfee9085f15c8dd43bc8e758bcaec222ac82
Tim Lister <tlister@lco.global>
2024-09-20 07:33:28 -0700

    Convert to python3 prints and 2 vs 3 configparser imports. Replaces tabs with spaces

--- devices/tecpak.py
+++ devices/tecpak.py
@@ -14,8 +14,14 @@ import time
 import ftplib
 import numbers
 import signal
-import ConfigParser
-cfg = ConfigParser.ConfigParser()
+import six
+if six.PY2:
+    import ConfigParser as ConfigParser
+    cfg = ConfigParser.ConfigParser()
+else:
+    import configparser
+    cfg = configparser.ConfigParser(inline_comment_prefixes=(';','#',))
+
 cfg.read('teckit.conf')
 cfg.sections()
 #from Adafruit_BME280 import *
@@ -74,14 +80,14 @@ class tec_meter():
 
     def __init__(self,gpib,reflevel,name):
         self.gpib = gpib
-        print "\033[4;5H \033[0;32mUART[\033[1m%2d\033[0;32m] : Arroyo TECpak\033[0;39m" % self.gpib
+        print("\033[4;5H \033[0;32mUART[\033[1m%2d\033[0;32m] : Arroyo TECpak\033[0;39m" % self.gpib)
         #if cfg.get('teckit', 'interface', 1) == 'gpib':
         #    self.inst = Gpib.Gpib(0, self.gpib, timeout = 180) # GPIB link
         #elif cfg.get('teckit', 'interface', 1) == 'vxi':
         #    self.inst = vxi11.Instrument(cfg.get('teckit', 'tec_ip', 1), "gpib0,%d" % self.gpib) # VXI link
         #    self.inst.timeout = 180
-	self.inst = serial.Serial(port='/dev/ttyS0', baudrate = 9600, parity=serial.PARITY_NONE, timeout=0.1)
-	self.reflevel = reflevel
+        self.inst = serial.Serial(port='/dev/ttyS0', baudrate = 9600, parity=serial.PARITY_NONE, timeout=0.1)
+        self.reflevel = reflevel
         self.name = name
         self.tec_rtd = tec_rtd
         self.init_inst()
@@ -114,7 +120,7 @@ class tec_meter():
         #self.inst.write(":sour:temp:prot:high 65")  #75C max temp
         #self.inst.write(":sour:temp:prot:low 5")   #15C min temp
         #self.inst.write(":sour:temp:prot:state ON") #enable temp #protection (default)
-	time.sleep(0.1)
+        time.sleep(0.1)
 
     def init_inst_ysi44006(self):
         # Setup SCPI DMM
@@ -124,14 +130,14 @@ class tec_meter():
         #Set temperature transducer to thermistor and 4-wire sense mode
         self.inst.write(":sour:func:mode temp")
         self.inst.write(":sens:temp:tran THER")      #select thermistor
-	self.inst.write(":SENS:TEMP:CURR 1e-4")
+        self.inst.write(":SENS:TEMP:CURR 1e-4")
         self.inst.write(":sens:temp:THER:RANG 1e4") #10 kOhm thermistor
         self.inst.write(":sens:temp:THER:A 1.032e-3") #A
         self.inst.write(":sens:temp:THER:B 2.387e-4") #B
         self.inst.write(":sens:temp:THER:C 1.580e-7") #C
         self.inst.write(":syst:rsen off")            #4-wire mode disabled
-	self.inst.write(":SYST:KEY 16")
-	self.inst.write(":SYST:KEY 16")
+        self.inst.write(":SYST:KEY 16")
+        self.inst.write(":SYST:KEY 16")
 
         #Set voltage limit
         self.inst.write(":sour:volt:prot 10.5") #+9.5V limit
@@ -144,30 +150,30 @@ class tec_meter():
 
     def set_gain(self, gain):
         #self.inst.write(":sour:temp:lcon:GAIN %6.4f" % gain)         # P
-	time.sleep(0.1)
+        time.sleep(0.1)
 
     def set_intg(self, intgr):
         #self.inst.write(":sour:temp:lcon:INT %5.4f" % intgr)          # I
-	time.sleep(0.1)
+        time.sleep(0.1)
 
     def set_derv(self, derv):
         #self.inst.write(":sour:temp:lcon:DER %5.4f" % derv)          # D
-	time.sleep(0.1)
+        time.sleep(0.1)
 
     def cfg_temp(self):
         self.inst.write("TEC:T 30.0\r") #set temp
-	time.sleep(0.8)
+        time.sleep(0.8)
         self.inst.write("TEC:OUT 1\r")
-	time.sleep(0.8)
+        time.sleep(0.8)
         tec_rtd = 30.0
 
     def off_temp(self):
         self.inst.write("TEC:OUT 0\r")
-	time.sleep(0.8)
+        time.sleep(0.8)
 
     def on_temp(self):
         self.inst.write("TEC:OUT 1\r")
-	time.sleep(0.8)
+        time.sleep(0.8)
 
     def set_tmp(self,tmp):
         string = float(tmp)
@@ -175,18 +181,18 @@ class tec_meter():
         #self.inst.write(":DISP:WIND2:TEXT:DATA \"SV %5.3f C, STEP %6d \";STAT ON;" % (tmp, cnt))
         #print ("Setting %2.1f" % string)
         self.inst.write("TEC:T %2.3f\r" % string) 
-	time.sleep(0.8)
+        time.sleep(0.8)
 
     def read_data(self,cmd):
         data_float = 0.0
         data_str = ""
-	time.sleep(0.1)
+        time.sleep(0.1)
         self.inst.write(cmd)
-	time.sleep(0.1)
+        time.sleep(0.1)
         try:
             with Timeout(20):
                 data_str = self.inst.read(self.inst.inWaiting())
-		time.sleep(0.1)
+                time.sleep(0.1)
 		#print "\033[30;5m%s\r\n" % data_str
         except Timeout.Timeout:
             print ("Timeout exception from dmm %s on read_data() inst.read()\n" % self.name)
